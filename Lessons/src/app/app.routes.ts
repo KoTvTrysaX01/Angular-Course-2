2654725bfc6d038d171b0a07f93a3580836c1f66
Vadim Elshin <elshinvadim@gmail.com>
2025-07-30 18:04:03 +0200

    Watched lesson 16 / Built-in-Pipes

--- Lessons/src/app/app.routes.ts
+++ Lessons/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
 import { StructuralDirectiveNgifVsIfComponent } from './structural-directive-ngif-vs-if/structural-directive-ngif-vs-if.component';
 import { AttributeDirectivesComponent } from './attribute-directives/attribute-directives.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { BuiltInPipesComponent } from './built-in-pipes/built-in-pipes.component';
 
 // Default route must be defined first
 // Wildcard route must be defined last
@@ -15,5 +16,6 @@ export const routes: Routes = [
     {path: 'data-binding', component: DataBindingComponent},
     {path: 'structural-directive', component: StructuralDirectiveNgifVsIfComponent},
     {path: 'attribute-directive', component: AttributeDirectivesComponent},
+    {path: 'built-in-pipes', component: BuiltInPipesComponent},
     {path: '**', component: PageNotFoundComponent}
 ];
