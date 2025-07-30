e605714d7985fd7af3e014ab37d11111cf2f43b4
Vadim Elshin <elshinvadim@gmail.com>
2025-07-31 01:25:51 +0200

    Watched lesson 18 / Pure & Impure Pipes

--- Lessons/src/app/built-in-pipes/built-in-pipes.component.ts
+++ Lessons/src/app/built-in-pipes/built-in-pipes.component.ts
@@ -2,12 +2,16 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { of } from 'rxjs';
 import { CustomPipe } from '../custom.pipe';
+import { PurePipe } from '../pure.pipe';
+import { ImpurePipe } from '../impure.pipe';
 
 @Component({
   selector: 'KOT-built-in-pipes',
   imports: [
     CommonModule,
-    CustomPipe
+    CustomPipe,
+    PurePipe,
+    ImpurePipe
   ],
   templateUrl: './built-in-pipes.component.html',
   styleUrl: './built-in-pipes.component.scss'
