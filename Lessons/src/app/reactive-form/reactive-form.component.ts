3525085b77a02fef012b15da2156f8368bac9e4d
Vadim Elshin <elshinvadim@gmail.com>
2025-08-01 14:18:52 +0200

    Watched lesson 22 / Reactive Form Validation

--- Lessons/src/app/reactive-form/reactive-form.component.ts
+++ Lessons/src/app/reactive-form/reactive-form.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'KOT-reactive-form',
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CommonModule
   ],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.scss'
@@ -13,21 +15,40 @@ export class ReactiveFormComponent {
 
   reactiveForm: FormGroup;
 
-  constructor(){
-    this.reactiveForm = new FormGroup({
-      firstName : new FormControl(""),
-      lastName : new FormControl(""),
-      email : new FormControl(""),
-      password : new FormControl(),
-      isChecked : new FormControl(true),
-      address: new FormGroup({
-        city: new FormControl(""),
-        street: new FormControl(""),
-        pincode: new FormControl()
-      })
+  constructor(private fb : FormBuilder){
+    this.reactiveForm = this.fb.group({
+      firstName : ['', [Validators.required, Validators.pattern("^[a-zA-Z]+$")]],
+      lastName : ['', [Validators.required, Validators.minLength(10)]],
+      email : ['', [Validators.required, Validators.maxLength(15), Validators.email]],
+      password : ['', [Validators.required]],
+      isChecked : [''],
+      address: this.fb.group({
+        city: [''],
+        street: [''],
+        pincode: ['']
+      }),
+      skills : this.fb.array([])
     })
 
     this.reactiveForm.controls['isChecked'].disable();
+  }
+
+  get skills() : FormArray{
+    return this.reactiveForm.get("skills") as FormArray;
+  }
+
+  newSkill(): FormGroup{
+    return this.fb.group({
+      skill : ''
+    })
+  }
+
+  addSkill(){
+    this.skills.push(this.newSkill())
+  }
+
+  deleteSkill(i:number){
+    this.skills.removeAt(i);
   }
 
   onSubmit(){
